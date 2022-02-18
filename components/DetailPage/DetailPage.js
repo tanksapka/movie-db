07c3d2df456f674b6cdab7acc047178c94de41c2
Tamas Schell <tanksapka@yahoo.com>
2022-02-18 01:14:16 +0100

    Adding profile picture for actors, tagline after the title and fixing actor name duplication.

--- components/DetailPage/DetailPage.js
+++ components/DetailPage/DetailPage.js
@@ -1,13 +1,22 @@
 import classes from "./DetailPage.module.css";
 import Image from "next/image";
 
-export default function DetailPage({ id, title, image, releaseDate, runtimeStr, plot, imDbRating, ...otherProps }) {
-  console.log(otherProps);
+export default function DetailPage({
+  id,
+  title,
+  image,
+  releaseDate,
+  runtimeStr,
+  plot,
+  imDbRating,
+  tagline,
+  ...otherProps
+}) {
   const rlsItems = releaseDate.split("-");
   const genreStr = otherProps.genreList.map((item) => item.value).join(", ");
   const directorItems = otherProps.directorList.map((person) => {
     return (
-      <div key={person.id} className={classes.creditsItem}>
+      <div key={`${person.id}__director`} className={classes.creditsItem}>
         <strong>{person.name}</strong>
         <p>Director</p>
       </div>
@@ -15,13 +24,33 @@ export default function DetailPage({ id, title, image, releaseDate, runtimeStr, 
   });
   const writerItems = otherProps.writerList.map((person) => {
     return (
-      <div key={person.id} className={classes.creditsItem}>
+      <div key={`${person.id}__writer`} className={classes.creditsItem}>
         <strong>{person.name}</strong>
         <p>Writer</p>
       </div>
     );
   });
   const creditItems = [...directorItems, ...writerItems];
+  const castList = otherProps.actorList
+    .filter((person, index) => index < 4)
+    .map((person) => {
+      return (
+        <div key={person.id} className={classes.castItem}>
+          <div className={classes.imgContainer}>
+            <Image
+              src={person.image}
+              alt={person.name}
+              width={96}
+              height={96}
+              layout="intrinsic"
+              objectFit="scale-down"
+            />
+          </div>
+          <strong>{person.name}</strong>
+          <p>{person.asCharacter.replace(/as \S.+/i, "")}</p>
+        </div>
+      );
+    });
 
   return (
     <main className={classes.detailContainer}>
@@ -29,12 +58,14 @@ export default function DetailPage({ id, title, image, releaseDate, runtimeStr, 
         <Image src={image} alt={title} width="580px" height="888px" />
       </section>
       <section className={classes.movieContainer}>
-        <h1>{title}</h1>
+        <div className={classes.titleContainer}>
+          <h1>{title}</h1>
+          <h2>&mdash; {tagline}</h2>
+        </div>
         <div className={classes.genericInfo}>
           <h6>{`${rlsItems[1]}/${rlsItems[2]}/${rlsItems[0]}`}</h6>
           <h6>{genreStr}</h6>
           <h6>{runtimeStr}</h6>
-          <h6>Trailer link</h6>
         </div>
         <div className={classes.imdbInfo}>
           <Image src="/imdb-icon.svg" alt="IMDb logo" width={64} height={32} />
@@ -48,24 +79,7 @@ export default function DetailPage({ id, title, image, releaseDate, runtimeStr, 
         </div>
         <div className={classes.creditsContainer}>{creditItems}</div>
         <hr className={classes.divider} />
-        <div className={classes.castContainer}>
-          <div className={classes.castItem}>
-            <strong>Clint Eastwood</strong>
-            <p>Harry Calahan</p>
-          </div>
-          <div className={classes.castItem}>
-            <strong>Hal Holbrook</strong>
-            <p>Lt. Briggs</p>
-          </div>
-          <div className={classes.castItem}>
-            <strong>Mitchell Ryan</strong>
-            <p>McCoy</p>
-          </div>
-          <div className={classes.castItem}>
-            <strong>David Soul</strong>
-            <p>Davis</p>
-          </div>
-        </div>
+        <div className={classes.castContainer}>{castList}</div>
       </section>
     </main>
   );
